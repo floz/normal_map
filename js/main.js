7f2fee755f65ce09b3de4586df4074cc50de3bf1
Florian Zumbrunn <florian.zumbrunn@nyc03fzumbrunn.local>
2013-08-27 15:35:27 -0400

    fin normal mapping with light + displacement

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 var LightConfiguration = function() {
-	this.shiny = 3;
-	this.specularity = 20;
-	this.lx = 50;
-	this.ly = 50;
-	this.lz = 100;
+	this.shiny = 10;
+	this.specularity = 15;
+	this.lx = -100;
+	this.ly = 100;
+	this.lz = 137;
 }
 
 var lightConf = new LightConfiguration();
@@ -13,21 +13,21 @@ gui.add( lightConf, "shiny", 0, 10 );
 gui.add( lightConf, "specularity", 0, 100 );
 gui.add( lightConf, "lx", -100, 100 );
 gui.add( lightConf, "ly", -100, 100 );
-gui.add( lightConf, "lz", -500, 500 );
+gui.add( lightConf, "lz", 0, 200 );
 
-//
-//
-//
+///
+///
+///
 
-var canvas = document.getElementById( "canvas_normal_map" );
-canvas.width = canvas.getAttribute( "width" );
-canvas.height = canvas.getAttribute( "height" );
-var ctx = canvas.getContext( "2d" );
+var canvasNormal = document.getElementById( "canvas_normal_map" );
+canvasNormal.width = canvasNormal.getAttribute( "width" );
+canvasNormal.height = canvasNormal.getAttribute( "height" );
+var ctxNormal = canvasNormal.getContext( "2d" );
 
 var normals = [];
-var normalData = getImageData( document.getElementById( "normal_map" ) ).data;
+var normalData = getImageData( document.getElementById( "normal_map" ), ctxNormal ).data;
 var nx, ny, nz, magInv;
-var n = canvas.width * canvas.height * 4;
+var n = canvasNormal.width * canvasNormal.height * 4;
 for( var i = 0; i < n; i+=4 )
 {
 	nx = normalData[ i ];
@@ -45,27 +45,23 @@ for( var i = 0; i < n; i+=4 )
 }
 
 
-var textureData = getImageData( document.getElementById( "texture" ) ).data;
-canvas.onmousemove = render;
-
-function getImageData( img ) {
-	ctx.clearRect( 0, 0, img.width, img.height );
-	ctx.drawImage( img, 0, 0 );
-	return ctx.getImageData( 0, 0, img.width, img.height );
-}
+var textureData = getImageData( document.getElementById( "texture" ), ctxNormal ).data;
+window.onmousemove = render;
 
 function render( e ) {
-	drawLight( lightConf.shiny, lightConf.specularity, e.clientX + lightConf.lx, e.clientY + lightConf.ly, lightConf.lz );
+	var cx = Math.max( 200, Math.min( e.clientX, 440 ) );
+	var cy = Math.max( 150, Math.min( e.clientY, 200 ) );
+	drawLight( lightConf.shiny, lightConf.specularity, cx + lightConf.lx, cy + lightConf.ly, lightConf.lz );
 }
 
 function drawLight( shiny, specularity, lx, ly, lz ) {
-	var imgData = ctx.getImageData( 0, 0, canvas.width, canvas.height );
+	var imgData = ctxNormal.getImageData( 0, 0, canvasNormal.width, canvasNormal.height );
 	var data = imgData.data;
 
 	var i = 0;
 	var ni = 0;
-	var nx = canvas.width;
-	var ny = canvas.height;
+	var nx = canvasNormal.width;
+	var ny = canvasNormal.height;
 	var dx = 0;
 	var dy = 0;
 	var dz = 0;
@@ -99,12 +95,49 @@ function drawLight( shiny, specularity, lx, ly, lz ) {
 			ni += 3;
 		}
 	}
-	ctx.putImageData( imgData, 0, 0 );
+	ctxNormal.putImageData( imgData, 0, 0 );
 }
 
 function clamp( x, min, max ) {
 	if( x < min ) return min;
 	if( x > max ) return max - 1;
 	return x;
+}
+
+///
+///
+///
+
+var displacementSource = document.getElementById( "canvas_displacement_source" );
+var displacementMap = document.getElementById( "canvas_displacement_map" );
+var displacementTarget = document.getElementById( "canvas_displacement_target" );
+
+fillContext( displacementSource, document.getElementById( "texture" ) );
+fillContext( displacementMap, document.getElementById( "displacement_map" ) );
+
+var filter = new filters.DisplacementMap( displacementSource, displacementMap, displacementTarget, new filters.Point(), 15, 15, filters.ColorChannelRED, filters.ColorChannelGREEN );
+filter.draw();
+
+function fillContext( canvas, img ) {
+	var ctx = canvas.getContext( "2d" );
+	ctx.drawImage( img, 0, 0, img.width, img.height );
+}
+
+gui = new dat.GUI();
+gui.add( filter, "scaleX", -100, 100 ).onChange( updateDisplacement );
+gui.add( filter, "scaleY", -100, 100 ).onChange( updateDisplacement );
+
+function updateDisplacement() {
+	filter.draw();
+}
+
+///
+///
+///
+
+function getImageData( img, ctx ) {
+	ctx.clearRect( 0, 0, img.width, img.height );
+	ctx.drawImage( img, 0, 0 );
+	return ctx.getImageData( 0, 0, img.width, img.height );
 }
 
